4cf2b1d2b501dd4837e2d493e59bb4d885506451
adammoy2 <musician903@gmail.com>
2016-11-15 09:36:45 -0600

    Added app.js, public/css
    Added app.js, home.html, package.json, and css files in public/css

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+var fs = require("fs");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+
+var homePage = fs.readFileSync("home.html");
+app.get("/", function (req, res) {
+    res.writeHeader(200, { "Content-Type": "text/html" });
+    res.end(homePage);
+});
+
+var searchPage = fs.readFileSync("public/html/search.html");
+app.get("/search", function(req, res) {
+    res.writeHeader(200, { "Content-Type": "text/html" });
+    res.end(searchPage);
+});
+
+app.post("/search", function(req, res) {
+    // TODO do something with POST
+    console.log(req.body);
+});
+
+var port = 8000;
+app.listen(port, function () {
+    console.log("Listening on port: " + port);
+});
