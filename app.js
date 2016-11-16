6387eecf376d5503a3d65fd7f0b9c002d5e47171
adammoy2 <musician903@gmail.com>
2016-11-16 10:31:02 -0600

    Moved html files to public/html
    Moved html files
    Added link to "/" when clicking logo
    Added header to all html except attended_events
    Modified app.js to redirect correctly
    Added placeholder buttons to go to various pages on homePage

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require("body-parser");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 
-var homePage = fs.readFileSync("home.html");
+var homePage = fs.readFileSync("public/html/home.html");
 app.get("/", function (req, res) {
     res.writeHeader(200, { "Content-Type": "text/html" });
     res.end(homePage);
@@ -24,10 +24,28 @@ app.post("/search", function(req, res) {
     console.log(req.body);
 });
 
-var eventPage = fs.readFileSync("public/html/event.html");
+var eventPage = fs.readFileSync("public/html/event_page.html");
 app.get("/event", function(req, res) {
     res.writeHeader(200, { "Content-Type": "text/html" });
     res.end(eventPage);
+});
+
+var attendedEventPage = fs.readFileSync("public/html/attended_events.html");
+app.get("/attendedevents", function(req, res) {
+    res.writeHeader(200, { "Content-Type": "text/html" });
+    res.end(attendedEventPage);
+});
+
+var loginPage = fs.readFileSync("public/html/login.html");
+app.get("/login", function(req, res) {
+    res.writeHeader(200, { "Content-Type": "text/html" });
+    res.end(loginPage);
+});
+
+var signupPage = fs.readFileSync("public/html/signup.html");
+app.get("/signup", function(req, res) {
+    res.writeHeader(200, { "Content-Type": "text/html" });
+    res.end(signupPage);
 });
 
 var port = 8000;
